4e6e457c1f11a3f53b772fec4844fa28bf0638fa
claws <jingjiecb@gmail.com>
2023-04-17 02:29:40 +0800

    feat: 舒适化maven版Spring测试项目

---
+++ springMavenBeginner/src/main/java/top/claws/Main.java
@@ -0,0 +1,11 @@
+package top.claws;
+
+/**
+ * @author claws
+ * @since 2023/4/17
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
